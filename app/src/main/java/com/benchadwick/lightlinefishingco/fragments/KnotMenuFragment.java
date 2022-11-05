5ce7fb37d408dff7b00d941545159f658de39e02
Ben Chadwick <benjamin.chadwick01@stclairconnect.ca>
2022-11-05 15:24:52 -0400

    Catch of the Day Fragment Created * Imported images to be used - #22 * created a pojo for the fish posts - #23 * created a layout file called recycle_row for recyclerview items - #24 * created a custom view adapter called FishRecyclerViewAdapter - #25

--- app/src/main/java/com/benchadwick/lightlinefishingco/fragments/KnotMenuFragment.java
+++ app/src/main/java/com/benchadwick/lightlinefishingco/fragments/KnotMenuFragment.java
@@ -1,4 +1,4 @@
-package com.benchadwick.lightlinefishingco;
+package com.benchadwick.lightlinefishingco.fragments;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.benchadwick.lightlinefishingco.R;
 
 /**
  * A simple {@link Fragment} subclass.
