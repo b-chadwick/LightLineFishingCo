83323d09b82fd9fbaa498766e7cd6fd04d6d4b6d
Ben Chadwick <benjamin.chadwick01@stclairconnect.ca>
2022-11-15 22:51:29 -0500

    knot tool finished viewpager now working fully #11

--- app/src/main/java/com/benchadwick/lightlinefishingco/knot_tool/CustomViewPageAdapter.java
+++ app/src/main/java/com/benchadwick/lightlinefishingco/knot_tool/CustomViewPageAdapter.java
@@ -30,7 +30,7 @@ public class CustomViewPageAdapter extends FragmentStateAdapter {
                knotInstructions.add(context.getString(R.string.clinch3));
                knotInstructions.add(context.getString(R.string.clinch4));
                knotInstructions.add(context.getString(R.string.clinch5));
-           break;
+            break;
             case "Palomar Knot":
                 knotImages.add(R.drawable.palomar1);
                 knotImages.add(R.drawable.palomar2);
@@ -46,6 +46,50 @@ public class CustomViewPageAdapter extends FragmentStateAdapter {
                 knotInstructions.add(context.getString(R.string.palomar5));
                 knotInstructions.add(context.getString(R.string.palomar6));
                 knotInstructions.add(context.getString(R.string.palomar7));
+            break;
+            case "Rapala Knot":
+                knotImages.add(R.drawable.rapala1);
+                knotImages.add(R.drawable.rapala2);
+                knotImages.add(R.drawable.rapala3);
+                knotImages.add(R.drawable.rapala4);
+                knotImages.add(R.drawable.rapala5);
+                knotImages.add(R.drawable.rapala6);
+                knotInstructions.add(context.getString(R.string.rapala1));
+                knotInstructions.add(context.getString(R.string.rapala2));
+                knotInstructions.add(context.getString(R.string.rapala3));
+                knotInstructions.add(context.getString(R.string.rapala4));
+                knotInstructions.add(context.getString(R.string.rapala5));
+                knotInstructions.add(context.getString(R.string.rapala6));
+            break;
+            case "Snell Knot":
+                knotImages.add(R.drawable.snell1);
+                knotImages.add(R.drawable.snell2);
+                knotImages.add(R.drawable.snell3);
+                knotImages.add(R.drawable.snell4);
+                knotImages.add(R.drawable.snell5);
+                knotImages.add(R.drawable.snell6);
+                knotImages.add(R.drawable.snell7);
+                knotInstructions.add(context.getString(R.string.snell1));
+                knotInstructions.add(context.getString(R.string.snell2));
+                knotInstructions.add(context.getString(R.string.snell3));
+                knotInstructions.add(context.getString(R.string.snell4));
+                knotInstructions.add(context.getString(R.string.snell5));
+                knotInstructions.add(context.getString(R.string.snell6));
+                knotInstructions.add(context.getString(R.string.snell7));
+            break;
+            case "Uni Knot":
+                knotImages.add(R.drawable.uni1);
+                knotImages.add(R.drawable.uni2);
+                knotImages.add(R.drawable.uni3);
+                knotImages.add(R.drawable.uni4);
+                knotImages.add(R.drawable.uni5);
+                knotImages.add(R.drawable.uni6);
+                knotInstructions.add(context.getString(R.string.uni1));
+                knotInstructions.add(context.getString(R.string.uni2));
+                knotInstructions.add(context.getString(R.string.uni3));
+                knotInstructions.add(context.getString(R.string.uni4));
+                knotInstructions.add(context.getString(R.string.uni5));
+                knotInstructions.add(context.getString(R.string.uni6));
             break;
         }
     }
